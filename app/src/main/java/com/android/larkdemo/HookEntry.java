da0b622cf1ea7313cd886f29b1289710174e3e02
hitwhoyxw <1634216025@qq.com>
2023-12-06 19:40:54 +0800

    实现对聊天窗口的红包自动点击的效果

--- app/src/main/java/com/android/larkdemo/HookEntry.java
+++ app/src/main/java/com/android/larkdemo/HookEntry.java
@@ -17,8 +17,11 @@ import android.widget.ImageView;
 import com.google.gson.Gson;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
+import dalvik.system.DexFile;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -59,64 +62,76 @@ public class HookEntry implements IXposedHookLoadPackage {
                             LogUtil.PrintLog(strMessage, "addMsg");
                         }
                     });
-
-                    findAndHookMethod("android.view.View", dexClassLoader, "performClick", new XC_MethodHook() {
+//                    findAndHookMethod("android.view.View", dexClassLoader, "performClick", new XC_MethodHook() {
+//                        @Override
+//                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+//                            Object mListenerInfo = XposedHelpers.getObjectField(param.thisObject, "mListenerInfo");
+//                            if (mListenerInfo != null) {
+//                                Object mOnClickListener = XposedHelpers.getObjectField(mListenerInfo, "mOnClickListener");
+//                                if (mOnClickListener != null) {
+//                                    String callbackClassName = mOnClickListener.getClass().getName();
+//                                    LogUtil.PrintLog("Callback class name: " + callbackClassName, "ImageViewClickHook");
+//                                    Log.i(TAG, "beforeHookedMethod: " + callbackClassName);
+//                                }
+//                            }
+//                        }
+//                    });
+                    findAndHookMethod(DexFile.class, "loadDex", String.class, String.class, int.class, new XC_MethodHook() {
                         @Override
                         protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            Object mListenerInfo = XposedHelpers.getObjectField(param.thisObject, "mListenerInfo");
-                            if (mListenerInfo != null) {
-                                Object mOnClickListener = XposedHelpers.getObjectField(mListenerInfo, "mOnClickListener");
-                                if (mOnClickListener != null) {
-                                    String callbackClassName = mOnClickListener.getClass().getName();
-                                    LogUtil.PrintLog("Callback class name: " + callbackClassName, "ImageViewClickHook");
-                                    Log.i(TAG, "beforeHookedMethod: " + callbackClassName);
-                                }
-                            }
+                            String dex = (String) param.args[0];
+                            String odex = (String) param.args[1];
+                            LogUtil.PrintLog("dex " + dex + "odex " + odex, "hook dexload");
                         }
                     });
-                 /*   findAndHookConstructor("com.ss.android.lark.money.redpacket.detail.RedpacketDetailView", dexClassLoader, new XC_MethodHook() {
-                        @Override
-                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                            LogUtil.PrintStackTrace(5);
-                        }
-                    });*/
+
                     try {
-                        findAndHookConstructor("android.view.View", dexClassLoader, new XC_MethodHook() {
-                            @Override
-                            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                                View view = (View) param.thisObject;
-                                String className = view.getClass().getName();
-                                if (!className.contains("RedpacketDetailView")) {
-                                    return;
-                                }
-                                LogUtil.PrintLog("find RedpacketDetailView", "*********************************************");
-                                strMessage = new Gson().toJson(param.args);
-                                LogUtil.PrintLog(strMessage, "view的参数");
-                                LogUtil.PrintStackTrace(5);
-                            }
-                        });
-                    } catch (XposedHelpers.ClassNotFoundError error) {
-                        LogUtil.PrintLog(error.toString(), "RedpacketDetailView");
-                    } finally {
+                        Class clzRedPacketMsgCell = XposedHelpers.findClass("com.ss.android.lark.chat.chatwindow.chat.message.cell.redpacket.RedPacketMessageCell", dexClassLoader);
+                        if (clzRedPacketMsgCell != null) {
+                            LogUtil.PrintLog("find RedPacketMessageCell!!!", "RedPacketMessageCell");
+                            Method targetMethod = null;
+                            for (Method m : clzRedPacketMsgCell.getDeclaredMethods()) {
+                                Boolean choise = m.getName().equals("a") && m.getParameterCount() == 3 && m.getParameterTypes()[0] == View.class && m.getParameterTypes()[1] == int.class && Modifier.isPublic(m.getModifiers());
+                                if (choise) {
+                                    LogUtil.PrintLog("find RedPacketMessageCell.a(View view, int i, com.ss.android.lark.chat.export.vo.c<com.ss.android.lark.chat.chatwindow.chat.view.message.plugin.redpackage.entity.a> cVar)!!!", "RedPacketMessageCell");
+                                    //通过限定找到了对应的a方法
+                                    XposedBridge.hookMethod(m, new XC_MethodHook() {
+                                        @Override
+                                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                                            View view = (View) param.args[0];
+                                            int i = (int) param.args[1];
+                                            LogUtil.PrintLog("args1 int = " + i, "RedPacketMessageCell");
 
-                    }
-                    findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
-                        @Override
-                        protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                            Activity activity = (Activity) param.thisObject;
-                            String activityName = activity.getClass().getName();
-                            LogUtil.PrintLog("onCreate hook success" + activityName, "Activity");
-                            if (activityName.contains("RedpacketDetailActivity")) {
-                                Bundle extras = (Bundle) param.args[0];
-                                if (extras != null) {
-                                    for (String key : extras.keySet()) {
-                                        Object value = extras.get(key);
-                                        LogUtil.PrintLog(value.toString(), "Bundle");
-                                    }
+                                            // 获取 ListenerInfo 对象
+                                            Field listenerInfoField = View.class.getDeclaredField("mListenerInfo");
+                                            listenerInfoField.setAccessible(true);
+                                            Object listenerInfo = listenerInfoField.get(view);
+
+                                            // 判断 ListenerInfo 对象是否为 null
+                                            if (listenerInfo != null) {
+                                                // 获取 OnClickListener 对象
+                                                Class<?> listenerInfoClass = Class.forName("android.view.View$ListenerInfo");
+                                                Field onClickListenerField = listenerInfoClass.getDeclaredField("mOnClickListener");
+                                                onClickListenerField.setAccessible(true);
+                                                Object onClickListener = onClickListenerField.get(listenerInfo);
+
+                                                if (onClickListener instanceof View.OnClickListener) {
+                                                    // 调用 OnClickListener 的 onClick 方法
+                                                    Method onClickMethod = View.OnClickListener.class.getDeclaredMethod("onClick", View.class);
+                                                    onClickMethod.setAccessible(true);
+                                                    onClickMethod.invoke(onClickListener, view);
+                                                    LogUtil.PrintLog("call onclick success", "RedPacketMessageCell");
+                                                }
+                                            }
+                                        }
+                                    });
                                 }
+
                             }
                         }
-                    });
+                    } catch (Exception e) {
+                        LogUtil.PrintLog(e.toString(), "RedPacketMessageCell");
+                    }
                 }
 
             });
